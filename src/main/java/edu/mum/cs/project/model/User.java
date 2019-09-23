8a9eeb8e915807992811c667d085fcca401f7d03
mniyonshuti <niyomoses1992@gmail.com>
2019-09-23 17:38:10 -0500

    create new user functionality

--- src/main/java/edu/mum/cs/project/model/User.java
+++ src/main/java/edu/mum/cs/project/model/User.java
@@ -1,5 +1,7 @@
 package edu.mum.cs.project.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,4 +18,106 @@ public class User {
     private List<Post> postList;
     private List<User> followingList = new ArrayList<>();
 
+    public User() {
+    }
+
+    public User(String finalName, String lastName, String email, String password,
+                LocalDate dateOfBirth, LocalDate dateOfRegistration) {
+        this.id = id;
+        this.finalName = finalName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfRegistration = dateOfRegistration;
+        this.postList = postList;
+        this.followingList = followingList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFinalName() {
+        return finalName;
+    }
+
+    public void setFinalName(String finalName) {
+        this.finalName = finalName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfRegistration() {
+        return dateOfRegistration;
+    }
+
+    public void setDateOfRegistration(LocalDate dateOfRegistration) {
+        this.dateOfRegistration = dateOfRegistration;
+    }
+
+    public List<Post> getPostList() {
+        return postList;
+    }
+
+    public void setPostList(List<Post> postList) {
+        this.postList = postList;
+    }
+
+    public List<User> getFollowingList() {
+        return followingList;
+    }
+
+    public void setFollowingList(List<User> followingList) {
+        this.followingList = followingList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", finalName='" + finalName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", dateOfRegistration=" + dateOfRegistration +
+                ", postList=" + postList +
+                ", followingList=" + followingList +
+                '}';
+    }
 }
